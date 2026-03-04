2677f995cbc48e1a1b5bb77d6fdfff2f51b5a54e
priyanshuTiwari-pt <chmcs2026.priyanshu.tiwari@gmail.com>
2026-03-04 21:46:09 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 connectDB();
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    //origin: "http://localhost:5173",
     credentials: true
 }));
 app.use(express.json());
